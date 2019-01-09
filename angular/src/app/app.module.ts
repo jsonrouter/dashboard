2853387b9b675852f0ade900b083a56917a1da15
Ben Wall <bwall256@gmail.com>
2019-01-09 14:04:27 +0000

    add test elements

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
+import { LayoutModule } from '@angular/cdk/layout';
+import { TestDragComponent } from './test-drag/test-drag.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainDashboardComponent,
+    TestDragComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
