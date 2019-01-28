f6c650f3e6d61f44a8415546cb17a2f6d352b124
alex breadman <alex.breadman@dianomi.com>
2019-01-28 13:18:40 +0000

    fix gen package

--- gen.go
+++ gen.go
@@ -92,6 +92,15 @@ func Generate() {
 		"ngsw-worker.js",
 		"safety-worker.js",
 		"worker-basic.min.js",
+		//
+		"icon-128x128.png",
+		"icon-144x144.png",
+		"icon-152x152.png",
+		"icon-192x192.png",
+		"icon-384x384.png",
+		"icon-512x512.png",
+		"icon-72x72.png",
+		"icon-96x96.png",
 	}
 	files := []string{}
 	decoders := []string{}
